0506ec17969912b2828c2ef893ff77eb5ff03aa5
Nycolas Darosci <nycoollaas@gmail.com>
2022-01-02 20:21:37 -0300

    variables and functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+# def FunctionName(Input):
+#     Action
+#     return Output
+
+def addOne(Number):
+    Number += 1
+    return Number
+
+
+number = 2
+
+var = addOne(number)
+
+print(var)
---
+++ variables.py
@@ -0,0 +1,20 @@
+f = 32
+Rf = 13
+
+a, b, c = 1, 2, 3
+print(a)
+a += 1
+print(a)
+j = f + Rf
+Var = "oi" + "1"
+
+def functionName():
+    #global f
+    NewVar = "oi"
+    print(NewVar)
+    print(f)
+    return 
+
+functionName()
+print(a, b, c)
+#print(Var)
